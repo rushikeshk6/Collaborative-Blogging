849e219951907ac01c32a5624b91d97ed943c632
rushikeshk6 <156289611+rushikeshk6@users.noreply.github.com>
2024-01-21 16:50:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use(cookieParser());
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
